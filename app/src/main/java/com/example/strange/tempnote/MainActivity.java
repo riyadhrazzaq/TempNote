a9bec578d72b4a151388f26e06bcc0d1add11532
riyadhrazzaq <riyadh.razzaq@gmail.com>
2018-07-10 03:13:21 +0600

    none done

--- app/src/main/java/com/example/strange/tempnote/MainActivity.java
+++ app/src/main/java/com/example/strange/tempnote/MainActivity.java
@@ -1,37 +1,65 @@
 package com.example.strange.tempnote;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView rv;
+    BaseFun fun = new BaseFun(this);
     private NoteAdapter mAdapter;
     private FloatingActionButton fab;
-    BaseFun fun = new BaseFun(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab = findViewById(R.id.fab);
-        RecyclerView rv = findViewById(R.id.oneRV);
+        // RecyclerView Initiate
+        rv = findViewById(R.id.oneRV);
         rv.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new NoteAdapter(this,fun.getAllByCursor());
+        mAdapter = new NoteAdapter(this, fun.getAllByCursor());
+
         rv.setAdapter(mAdapter);
 
+
+
+        //FloatingActionBar initiate
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                startActivity(new Intent(getApplicationContext(),Write.class));
+                startActivity(new Intent(getApplicationContext(), Write.class));
             }
         });
+        //Remove on swipe for RecyclerView
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                fun.removeById(Integer.parseInt(viewHolder.itemView.getTag().toString()));
+                mAdapter.notifyDataSetChanged();
+
+                recreate();
+
+            }
+        }).attachToRecyclerView(rv);
+
+
+
     }
 }
--- app/src/main/java/com/example/strange/tempnote/NoteAdapter.java
+++ app/src/main/java/com/example/strange/tempnote/NoteAdapter.java
@@ -1,21 +1,24 @@
 package com.example.strange.tempnote;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by riyadh on 08-Jul-18.
  */
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> implements View.OnClickListener {
 
-    private Context mContext;
-    private Cursor mCursor;
+    public Context mContext;
+    public Cursor mCursor;
+    String title,note,exp;
 
 
     public NoteAdapter(Context context, Cursor cursor){
@@ -23,16 +26,35 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
             mContext = context;
     }
 
+
     public class NoteViewHolder extends RecyclerView.ViewHolder {
 
         public TextView titleTV, noteTV, expTV;
 
         public NoteViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCursor.moveToPosition(getAdapterPosition());
+                    Intent intent = new Intent(mContext,display.class);
+                    intent.putExtra("title",mCursor.getString(1));
+                    intent.putExtra("title",mCursor.getString(2));
+                    intent.putExtra("title",mCursor.getString(3));
+                    mContext.startActivity(intent);
+                    Toast.makeText(mContext,String.valueOf(getAdapterPosition()),Toast.LENGTH_SHORT).show();
+
+                }
+            });
             titleTV = itemView.findViewById(R.id.titleTV);
             noteTV = itemView.findViewById(R.id.noteTV);
             expTV = itemView.findViewById(R.id.expTV);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 
     @Override
@@ -55,15 +77,17 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         holder.titleTV.setText(title);
         holder.noteTV.setText(note);
         holder.expTV.setText(exp);
+        holder.itemView.setTag(mCursor.getString(0));
     }
 
     @Override
     public int getItemCount() {
         return mCursor.getColumnCount();
     }
+
     public void swapCursor (Cursor newcursor){
         if(mCursor!=null) mCursor.close();
         mCursor = newcursor;
-        if(newcursor!=null) notifyDataSetChanged();
+
     }
 }
